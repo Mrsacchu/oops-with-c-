69138b6e68aa42de534e4f1009a3268e1155d106
Mrsacchu <147688912+Mrsacchu@users.noreply.github.com>
2024-03-12 23:09:19 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+int marks[4] ={4,5,6,7};
+ cout<<"numbe r of array:"<<marks[0]<<"\n";
+cout<<"number of array:"<<marks[1]<<"\n";
+
+cout<<"number of array:"<<marks[2]<<"\n";
+
+cout<<"number of array:"<<marks[3]<<"\n";
+
+for(int i=0;i<4;i++){
+        cout<<"thise are the marks:";
+        cout<<marks[i]<<"\n";
+}
+
+
+}
